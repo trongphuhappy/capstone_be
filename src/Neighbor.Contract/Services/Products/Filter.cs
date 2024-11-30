e1c4793ba7af40b388a4ac0592f9d088b23432ec
Judar <trongphu012345@gmail.com>
2024-11-30 18:22:59 +0700

    Add IsVehicle to FilterParams in GetAllProducts.

--- src/Neighbor.Contract/Services/Products/Filter.cs
+++ src/Neighbor.Contract/Services/Products/Filter.cs
@@ -4,7 +4,7 @@ namespace Neighbor.Contract.Services.Products;
 
 public static class Filter
 {
-    public record ProductFilter(Guid? Id, string? Name, StatusType? StatusType, string? Policies, string? Description, double? Rating, double? Price, double? Value, int? MaximumRentDays, int? CategoryId, ConfirmStatus? ConfirmStatus, Guid? AccountUserId, Guid? AccountLessorId);
+    public record ProductFilter(Guid? Id, string? Name, StatusType? StatusType, string? Policies, string? Description, double? Rating, double? Price, double? Value, int? MaximumRentDays, int? CategoryId, ConfirmStatus? ConfirmStatus, bool? IsVehicle, Guid? AccountUserId, Guid? AccountLessorId);
 
     public record ProductWishlistFilter(Guid? Id, string? Name, StatusType? StatusType, string? Policies, string? Description, double? Rating, double? Price, double? Value, int? MaximumRentDays, int? CategoryId);
 }
